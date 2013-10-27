7568cb90ac8aff95cd005ccca4d4c1160c9722a0
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-10-27 15:30:19 +0000

    separate sample measurement app for network-based localization

---
+++ measurements/network/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/network/MainActivity.java
@@ -0,0 +1,49 @@
+package uk.ac.st_andrews.cs.host.mk74.energymeasurement.network;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+
+class EmptyLocationListener implements LocationListener{
+	@Override
+	public void onLocationChanged(Location loc) {}
+
+	@Override
+	public void onProviderDisabled(String arg0) {}
+
+	@Override
+	public void onProviderEnabled(String arg0) {}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
+}
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		
+		LocationListener locListener = new EmptyLocationListener();
+		
+		String locProvider = LocationManager.NETWORK_PROVIDER;
+		locManager.requestLocationUpdates(locProvider, 0, 0, locListener);
+		System.out.println("Location listener is registered\n");
+		
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
