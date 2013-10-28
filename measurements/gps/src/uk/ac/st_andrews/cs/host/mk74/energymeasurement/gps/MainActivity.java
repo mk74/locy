b9a43f44aee3646b994ab5b7631ad47dbbd2d775
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-10-28 10:41:15 +0000

    prating results n screen for GPS measurements

--- measurements/gps/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/gps/MainActivity.java
+++ measurements/gps/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/gps/MainActivity.java
@@ -7,20 +7,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
-
-class EmptyLocationListener implements LocationListener{
-	@Override
-	public void onLocationChanged(Location loc) {}
-
-	@Override
-	public void onProviderDisabled(String arg0) {}
-
-	@Override
-	public void onProviderEnabled(String arg0) {}
-
-	@Override
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
-}
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -30,6 +17,25 @@ public class MainActivity extends Activity {
 		
 		LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		
+		class EmptyLocationListener implements LocationListener{
+			
+			@Override
+			public void onLocationChanged(Location loc) {				
+				TextView tv = (TextView) findViewById(R.id.textView1);
+				tv.setText("Position: " + loc.getLongitude() + " " + loc.getLatitude() + "\n");
+				System.out.println("Position: " + loc.getLongitude() + " " + loc.getLatitude() + "\n");
+			}
+
+			@Override
+			public void onProviderDisabled(String arg0) {}
+
+			@Override
+			public void onProviderEnabled(String arg0) {}
+
+			@Override
+			public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
+		}
+
 		LocationListener locListener = new EmptyLocationListener();
 		
 		String locProvider = LocationManager.GPS_PROVIDER;
