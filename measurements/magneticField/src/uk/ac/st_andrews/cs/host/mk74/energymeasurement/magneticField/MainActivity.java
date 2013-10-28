e422bb9fefda4f7ebe7ddcd7ce70e1fb5cf97059
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-10-28 11:59:56 +0000

    sample magnetic field app for energy measurements

---
+++ measurements/magneticField/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/magneticField/MainActivity.java
@@ -0,0 +1,48 @@
+package uk.ac.st_andrews.cs.host.mk74.energymeasurement.magneticField;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		SensorManager mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		Sensor mMagneticField= mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		
+		class EmptySensorEventListener implements SensorEventListener{
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				TextView tv = (TextView) findViewById(R.id.textView1);
+				String output = "Values: " + event.values[0] + " " + event.values[1] + " " + event.values[2] +"\n";
+				tv.setText(output);
+				System.out.println(output);
+			}
+			
+		}
+		
+		mSensorManager.registerListener(new EmptySensorEventListener(), mMagneticField, 0);
+		
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
