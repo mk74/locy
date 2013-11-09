86701f0e318598800ff6aee2004ea23d9cded221
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-11-09 21:34:39 +0000

    batching accelerometer fixtures, calculating average values over them and calculating euclidean distance between current batched fixtures' average and old one

--- locy/src/uk/ac/st_andrews/cs/host/mk74/locy/LocyActivityRecognition.java
+++ locy/src/uk/ac/st_andrews/cs/host/mk74/locy/LocyActivityRecognition.java
@@ -1,5 +1,8 @@
 package uk.ac.st_andrews.cs.host.mk74.locy;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -7,6 +10,9 @@ import android.hardware.SensorManager;
 
 public class LocyActivityRecognition {
 	LocyManager locyManager;
+	float[] averageOldValues = null;
+	ArrayList<float[]> lastValues = new ArrayList<float[]>(); 
+	static int BATCH_SIZE = 20;
 	
 	public LocyActivityRecognition(LocyManager locyManager) {
 		this.locyManager = locyManager;
@@ -23,13 +29,50 @@ public class LocyActivityRecognition {
 
 			@Override
 			public void onSensorChanged(SensorEvent event) {
-				String output = "Values: " + event.values[0] + " " + event.values[1] + " " + event.values[2] +"\n";
-				LocyEvent locyEvent = new LocyEvent(output);
-				locyManager.locyEventListener.onLocyChanged(locyEvent);
+				float[] averageValues;
+				String output="";
+				
+				if(lastValues.size()<BATCH_SIZE)
+					lastValues.add(event.values.clone());
+				else{
+					averageValues = calcAverageOldValues();
+					lastValues.clear();
+					if(averageOldValues != null){
+						output = "Distance:"+ calcEuclideanDistance(averageOldValues, averageValues);
+						LocyEvent locyEvent = new LocyEvent(output);
+						locyManager.locyEventListener.onLocyChanged(locyEvent);
+					}
+					averageOldValues = averageValues;
+				}
 			}
 		}
 		int rate = 1000000 * 1000000;
 		mSensorManager.registerListener(new ARSensorEventListener(), mAccelerometer, rate);
+	}
+	
+	private float[] calcAverageOldValues(){
+		float[] avg = new float[3];
+		for(int i=0; i<3; i++)
+			avg[i] = (float) 0.0;
+		
+		Iterator<float[]> it = lastValues.iterator();
+		while(it.hasNext()){
+			float[] tmp = it.next();
+			for(int i=0; i<3; i++)
+				avg[i]+= tmp[i];
+		}
+		
+		for(int i=0; i<3; i++)
+			avg[i] = avg[i]/lastValues.size();
+		return avg;
+	}
+	
+	private static int calcEuclideanDistance(float[] values1, float[] values2) {
+		double sum = 0.0;
+		for(int i=0;i<3;i++){
+			sum += Math.pow(values1[i] - values2[i], 2.0);
+		}
+		return (int) Math.sqrt(sum);
 	}
 	
 }
