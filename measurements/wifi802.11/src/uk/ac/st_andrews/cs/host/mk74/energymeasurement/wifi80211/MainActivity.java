3ae2561dc0cf963dc75e2b866ff1b94d7319f412
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-11-13 20:03:40 +0000

    sample app scanning wifi hot spots every 5 secs and printing it to console. Only data is retrived(scan list), no information is provided(no location)

---
+++ measurements/wifi802.11/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/wifi80211/MainActivity.java
@@ -0,0 +1,59 @@
+package uk.ac.st_andrews.cs.host.mk74.energymeasurement.wifi80211;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		TimerTask scanNetworksTask = new TimerTask() {
+			
+			@Override
+			public void run() {
+				WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+				wifiManager.startScan();
+				List<ScanResult> scans = wifiManager.getScanResults();
+				
+				StringBuffer buff = new StringBuffer("");
+				Iterator<ScanResult> it = scans.iterator();
+				while(it.hasNext()){
+					ScanResult scanResult = it.next();
+					buff.append("SSID: " + scanResult.SSID+ "\tBSSID:" + scanResult.BSSID + "\tcapabilites" + scanResult.capabilities);
+					buff.append("\tfrequency:" + scanResult.frequency + "\tlevel:" + scanResult.level + "\n"); 
+				}
+				System.out.println(buff.toString());
+			}
+		};
+		Timer timer = new Timer();
+		long delay = 5 * 1000;
+		long period = 5 * 1000;
+		timer.schedule(scanNetworksTask, delay, period);
+
+		Window w = getWindow();
+		w.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, 
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
