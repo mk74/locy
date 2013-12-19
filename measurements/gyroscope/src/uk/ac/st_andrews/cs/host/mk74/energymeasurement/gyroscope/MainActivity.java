f678bdd03c041035c3fbec021864444e9f26465b
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-12-19 14:40:05 +0000

    new method of energy measurement: check how fast one percentage of battery is depleted [gyroscope]

--- measurements/gyroscope/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/gyroscope/MainActivity.java
+++ measurements/gyroscope/src/uk/ac/st_andrews/cs/host/mk74/energymeasurement/gyroscope/MainActivity.java
@@ -1,31 +1,41 @@
 package uk.ac.st_andrews.cs.host.mk74.energymeasurement.gyroscope;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	final private static int BATTERY_LEVEL_START=99;
+	final private static int BATTERY_LEVEL_END=98;
+	final private static int SEC_FREQUENCY = 1;
+	
 	public float[] values;
+	public Date batteryLevelStartTime, batteryLevelEndTime;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		//register listener for gyroscope data and save it
 		values = new float[]{(float) 0.0, (float) 0.0, (float) 0.0};
 		SensorManager mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		Sensor mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-		
 		class EmptySensorEventListener implements SensorEventListener{
-
+			
 			@Override
 			public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
@@ -36,21 +46,55 @@ public class MainActivity extends Activity {
 			}
 			
 		}
-		
 		int rate = 0;
 		mSensorManager.registerListener(new EmptySensorEventListener(), mGyroscope, rate);
 		
+		//print results every sec and check battery life whether the defined range was depleted
 		TimerTask readValues = new TimerTask() {
 			
 			@Override
 			public void run() {
 				String output = "Values: " + values[0] + " " + values[1] + " " + values[2] +"\n";
 				System.out.println(output);
+				
+				IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+				Intent batteryStats = registerReceiver(null, ifilter);
+				int level = batteryStats.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+				if(level == BATTERY_LEVEL_START && batteryLevelStartTime == null){
+					final String batteryOutput = "Battery level start is reached\n";
+					System.out.println(batteryOutput);
+					batteryLevelStartTime = new Date();
+					runOnUiThread(new Runnable(){
+
+						@Override
+						public void run() {
+							TextView tv = (TextView) findViewById(R.id.textView1);
+							tv.setText(batteryOutput);
+						}
+					});
+				}
+				if(level == BATTERY_LEVEL_END && batteryLevelEndTime == null){
+					System.out.println("Battery level end is reached\n");
+					batteryLevelEndTime = new Date();
+				}
+				if(batteryLevelStartTime != null && batteryLevelEndTime != null){
+					int diffInSecs = (int) ((batteryLevelEndTime.getTime() - batteryLevelStartTime.getTime()) / 1000);
+					final String batteryOutput = "Time difference in secs is: " + diffInSecs + "\n";
+					System.out.println(batteryOutput);
+					runOnUiThread(new Runnable(){
+
+						@Override
+						public void run() {
+							TextView tv = (TextView) findViewById(R.id.textView1);
+							tv.setText(batteryOutput);
+						}
+					});
+				}
 			}
 		};
 		Timer timer = new Timer();
-		long delay = 5 * 1000;
-		long period = 5 * 1000;
+		long delay = SEC_FREQUENCY * 1000;
+		long period = SEC_FREQUENCY * 1000;
 		timer.schedule(readValues, delay, period);
 		
 		Window w = getWindow();
