6e03898b1ae4982d778ba9207c656d525eefb440
Martin M. Kukla <mk74@st-andrews.ac.uk>
2014-01-19 16:57:50 +0000

    NaiveWifiIndoorLocalization which serves as baseline implementation of indoor localization - the one against which I am going to compare mine

---
+++ NaiveWifiIndoorLocalization/src/uk/ac/st_andrews/cs/host/mk74/naivewifiindoorlocalization/MainActivity.java
@@ -0,0 +1,67 @@
+package uk.ac.st_andrews.cs.host.mk74.naivewifiindoorlocalization;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	public static double SCREEN_UDPATE_FREQUENCY = 0.5;
+
+	WiFiNavigator wifiNavigator;
+	BatteryEvaluator batteryEvaluator;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//set up components: WiFiNavigator and BatteryEvaluator
+		wifiNavigator = new WiFiNavigator(getApplicationContext());
+		batteryEvaluator = new BatteryEvaluator(getApplicationContext());
+		wifiNavigator.start();
+		batteryEvaluator.enable();
+		
+		//prepare screen/layout
+		Window w = getWindow();
+		w.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, 
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.activity_main);
+		
+		//print(screen & logcat) state of Navigator:
+		//current localization, whether WiFiNavigator is enabled and battery info(level + time difference)
+		TimerTask readValues = new TimerTask() {
+			@Override
+			public void run() {
+				final String output = "Location: " + wifiNavigator.getLocation() + " | " +
+								"WiFiNavigator running:" +wifiNavigator.isRunning()+ "\n" +
+								"Battery level: " + batteryEvaluator.getBatteryLevel() + " | " +
+								"Time difference: " + batteryEvaluator.getTimeDifference() + "\n";
+				System.out.println(output);
+				runOnUiThread(new Runnable(){
+                    @Override
+                    public void run() {
+                    	TextView tv = (TextView) findViewById(R.id.textView1);
+                    	tv.setText(output);
+                    }
+				});
+			}
+		};
+		Timer timer = new Timer();
+		long delay = (long) (SCREEN_UDPATE_FREQUENCY * 1000);
+		long period = (long) (SCREEN_UDPATE_FREQUENCY * 1000);
+		timer.schedule(readValues, delay, period);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ NaiveWifiIndoorLocalization/src/uk/ac/st_andrews/cs/host/mk74/naivewifiindoorlocalization/WiFiNavigator.java
@@ -0,0 +1,138 @@
+package uk.ac.st_andrews.cs.host.mk74.naivewifiindoorlocalization;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+
+//WiFiNavigator
+//Scans available networks and send it over the network to fetch current mobile phone's localization
+//
+// New scan is triggered once previous one is finished(continuous scanning)
+// Sending over scan results happen as often as new ones are available
+// Assumes that WiFi is enabled
+public class WiFiNavigator {
+	public static String WIFI_FINGERPRINTS_SERVER = "http://mk74.host.cs.st-andrews.ac.uk/locy/location.php";
+	public static String WIFI_FINGERPRINTS_SERVER_VAR = "wifi_scan_results";
+	
+	private boolean running = false;
+	private WifiManager wifiManager;
+	private IntentFilter wifiIntentFilter;
+	private BroadcastReceiver wifiBroadcastReceiver;
+	private Context context;
+	private String location;
+		
+	public WiFiNavigator(Context context) {
+		this.context = context;
+		wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		location = "unknown";
+		
+		//intent and receiver for scheduling new scan when previous finished & sending results to get current location
+		wifiIntentFilter = new IntentFilter();
+		wifiIntentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+		wifiBroadcastReceiver= new BroadcastReceiver(){		
+			@Override
+			public void onReceive(Context c, Intent i){
+				new SendScanResultsTask().execute();
+				wifiManager.startScan();
+		    }
+		};
+	}
+	
+	//extract localization from web service response
+	private void extractLocalization(InputStream in) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		try {
+			location = reader.readLine(); // assumes that output is just one line
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	//create String containing list of all available networks with BSSID, SSID and its strength level
+	private String constructScanResuls() {
+		StringBuffer buff = new StringBuffer();
+		List<ScanResult> scanResults = wifiManager.getScanResults();
+		for(ScanResult scanResult : scanResults){
+			buff.append(scanResult.BSSID + " | " + scanResult.SSID + " | " + scanResult.level);
+		}
+		return buff.toString();
+	}
+	
+	public void start() {
+		if(!running){
+			running = true;
+			context.registerReceiver(wifiBroadcastReceiver, wifiIntentFilter);
+			wifiManager.startScan();
+		}
+	}
+	
+	public void stop() {
+		running = false;
+		context.unregisterReceiver(wifiBroadcastReceiver);
+	}
+	
+	public boolean isRunning() {
+		return running;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	//Sends WifiScan results over the network to get current location
+	private class SendScanResultsTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			//construct POST data with Wifi scan results
+			String results = constructScanResuls();
+			String post_data = "";
+			try {
+				post_data = URLEncoder.encode(WIFI_FINGERPRINTS_SERVER_VAR, "UTF-8") + "=" + 
+								 	URLEncoder.encode(results, "UTF-8");
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			//send those scan results and get the response where user is located
+			try {
+				URL url = new URL(WIFI_FINGERPRINTS_SERVER);
+				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestMethod("POST");
+				urlConnection.setDoInput(true);
+				urlConnection.setDoOutput(true);
+				
+				OutputStream os = urlConnection.getOutputStream();
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+				writer.write(post_data);
+				writer.flush();
+				writer.close();
+				
+				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+				extractLocalization(in);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}	
+	}
+}
