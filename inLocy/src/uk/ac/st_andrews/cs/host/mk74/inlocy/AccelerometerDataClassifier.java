364b992ac76e943a6c9b9559c7b7753b6041292a
Martin M. Kukla <mk74@st-andrews.ac.uk>
2014-01-22 18:18:33 +0000

    polishing algorithm for movement recognition. Works correctly for Google Nexus 7. However, we need concept of time as amount of accelerometer data fetched per sec differs across devices

--- inLocy/src/uk/ac/st_andrews/cs/host/mk74/inlocy/AccelerometerDataClassifier.java
+++ inLocy/src/uk/ac/st_andrews/cs/host/mk74/inlocy/AccelerometerDataClassifier.java
@@ -3,8 +3,14 @@ package uk.ac.st_andrews.cs.host.mk74.inlocy;
 import java.util.ArrayList;
 
 public class AccelerometerDataClassifier {
-	private static final double MOVEMENT_THRESHOLD = 25;
-	private static final int WINDOW_SIZE = 1000;
+	private static final double MOVEMENT_THRESHOLD = 15.0;
+	private static final int WINDOW_SIZE = 550;
+	private static final int WINDOW_N = 3; 
+	
+	private int predictedMoving = 0;
+	private int leftWindowN = WINDOW_N;
+	
+	private double lastStandardDeviation = 0.0; //only for debugging(printing values in real time)
 	
 	private boolean moving=true;
 	ArrayList<Double> magnitudes = new ArrayList<Double>();
@@ -13,9 +19,21 @@ public class AccelerometerDataClassifier {
 		//todo race condtion
 		magnitudes.add(calcMagnitude(values));
 		if(magnitudes.size() == WINDOW_SIZE){
-			System.out.println("FULL WINDOW!");
+			System.out.println("FULL WINDOW");
 			classify();
 			magnitudes.clear();
+			
+			leftWindowN--;
+			if(leftWindowN == 0){
+				if(predictedMoving>0){
+					moving = true;
+				}
+				if(predictedMoving<0){
+					moving = false;
+				}
+				predictedMoving = 0;
+				leftWindowN = WINDOW_N;
+			}
 		}
 		return moving;
 	}
@@ -30,11 +48,13 @@ public class AccelerometerDataClassifier {
 	private void classify() {
 		double mean = calcMean();
 		double standard_deviation = calcStandardDeviation(mean);
-		if(standard_deviation > MOVEMENT_THRESHOLD)
-			moving = true;
-		else
-			moving = false;
+		lastStandardDeviation = standard_deviation;
+		System.out.println("Standard deviation: " + standard_deviation);
 		
+		if(standard_deviation > MOVEMENT_THRESHOLD)
+			predictedMoving++;
+		else
+			predictedMoving--;
 	}
 
 	private double calcStandardDeviation(double mean) {
@@ -54,6 +74,12 @@ public class AccelerometerDataClassifier {
 		}
 		mean /= magnitudes.size();
 		return mean;
+	}
+
+	public String getInfo() {
+		double rounded = (double) Math.round(lastStandardDeviation * 100) / 100;
+		return "isMoving: " + moving + " | predictableMoving: " + predictedMoving + 
+				" | left window n: " + leftWindowN + " | last stddev: " + rounded + "\n";
 	}
 	
 }
